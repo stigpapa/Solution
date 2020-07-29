1cb463e22185a3d394c4531ebbe40fea98eb191d
Nathan Stigler <nathan.stigler@badger.k12.ar.us>
2020-07-29 15:39:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+  //This is a solution to a program.
+  int count = 0;
+  for(int x = 1; x < 1000; x++){
+    if (x % 3 == 0 || x % 5 == 0){
+      count = count + x;
+    }
+    
+  }
+System.out.println("The sum of all intergers under 10 that are divisible by 3 and 5 is " + count);
+  }
+}
